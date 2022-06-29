2e163cd0792bc55f7584250fbb8a10439e48892c
Abderazzak Oumad <aoumad@e3r12p4.1337.ma>
2022-06-29 14:11:22 +0100

    done

--- srcs/builtins/builtin_pwd.c
+++ srcs/builtins/builtin_pwd.c
@@ -6,49 +6,34 @@
 /*   By: aoumad <abderazzakoumad@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 09:50:51 by aoumad            #+#    #+#             */
-/*   Updated: 2022/06/29 01:36:58 by aoumad           ###   ########.fr       */
+/*   Updated: 2022/06/29 13:37:13 by aoumad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void	rm_case()
+int	builtin_pwd(int argc __attribute((unused)),
+	char **argv __attribute((unused)))
 {
 	int	i;
 
 	i = 0;
-	while (g_env[i])
-	{
-		if (!ft_strncmp(g_env[i], "PWD", 3))
-			printf("%s\n", g_env[i] + 4);
-		i++;
-	}
-}
-
-void	print_pwd()
-{
-	char	buf[PATH_MAX];
-
+	char buf[MAX_BUF];
 	if (getcwd(buf, sizeof(buf)))
 	{
 		ft_putendl_fd(buf, STDOUT_FILENO);
+		return (0);
 	}
-}
-
-int	builtin_pwd(int argc __attribute((unused)),
-	char **argv __attribute((unused)))
-{
-	int status;
-	
-	status = 0;
-	if (!getcwd(NULL, 0))
+	else if (!getcwd(NULL, 0))
 	{
-		status = 1;
-		rm_case();
+		while (g_env[i])
+		{
+			if (!ft_strncmp(g_env[i], "PWD", 3))
+			{
+				printf("%s\n", g_env[i] + 4);
+			}
+			i++;
+		}
 	}
-	if (status == 0)
-	{
-		print_pwd();
-	}
-	return (status);
+	return (0);		
 }
